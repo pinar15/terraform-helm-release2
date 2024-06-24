9c1f520f829a111d0046278e94a4f7d723d036be
pinar15 <pdasdemir49.gmail.com>
2024-06-24 15:09:32 -0500

    releasing version3

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-module "app"  {
-  source    = "./module"
+module "wordpress"  {
+  source    = "pinar15/release2/helm"
   namespace = "default"
   name      = "wordpress"
   wait      = false
   chart     = "./application"
-  values = [<<EOF
-replicaCount: 3
-image:
-  repository: wordpress
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-  EOF
-  ]
+  values = []
 }
 
 
